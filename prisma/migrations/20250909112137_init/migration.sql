da51f9840115cf76fda4c1a44143892018da2158
Siddhesh <srsiddheshrane@gmail.com>
2025-09-09 17:00:32 +0530

    resolved db connection issue

---
+++ prisma/migrations/20250909112137_init/migration.sql
@@ -0,0 +1,40 @@
+-- CreateEnum
+CREATE TYPE "public"."NotificationType" AS ENUM ('USER_GENERATED', 'SYSTEM_GENERATED', 'SCHEDULED');
+
+-- CreateEnum
+CREATE TYPE "public"."NotificationStatus" AS ENUM ('UNREAD', 'READ');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Notification" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "type" "public"."NotificationType" NOT NULL,
+    "status" "public"."NotificationStatus" NOT NULL DEFAULT 'UNREAD',
+    "scheduledAt" TIMESTAMP(3),
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Notification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "public"."User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- AddForeignKey
+ALTER TABLE "public"."Notification" ADD CONSTRAINT "Notification_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- router/userRouter/userRouter.js
+++ router/userRouter/userRouter.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
 
 
-router.get("/",(req,res)=>{
-    res.json({message:"all users fetched successfully"});
+router.get("/",async (req,res)=>{
+
+    const allUsers = await prisma.user.findMany()
+
+    res.json({message:"all users fetched successfully",users:allUsers});
 });
 
 
