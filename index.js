7e6d0cc8971b8ce1fe100ea26895d92b4205f112
Siddhesh <srsiddheshrane@gmail.com>
2025-09-13 23:42:57 +0530

    redis connection completed

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const port = process.env.PORT;
 const cors = require('cors');
 const router = require('./router/router.js');
 const connectDB = require('./config/mongodbConfig.js');
+const connectRedis = require("./config/redisConfig.js");
 
 
 app.use(cors({origin:"http://localhost"}));
 app.use(express.json()); 
 app.use('/api',router);
 connectDB();
+let redis;
+connectRedis().then(client => {
+  redis = client;
+});
 
 app.get('/',(req,res)=>{
 	
