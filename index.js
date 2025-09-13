f1e28e507fde4cd187cd1e320af3a0fc43b7c72c
Siddhesh <srsiddheshrane@gmail.com>
2025-09-13 15:36:12 +0530

    Added Mongodb

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ require('dotenv').config();
 const port = process.env.PORT;
 const cors = require('cors');
 const router = require('./router/router.js');
+const connectDB = require('./config/mongodbConfig.js');
+
 
 app.use(cors({origin:"http://localhost"}));
 app.use(express.json()); 
 app.use('/api',router);
-
+connectDB();
 
 app.get('/',(req,res)=>{
 	
--- router/userRouter/userRouter.js
+++ router/userRouter/userRouter.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { PrismaClient } = require('@prisma/client');
+//const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient();
+//const prisma = new PrismaClient();
 
 
 router.get("/",async (req,res)=>{
 
-    const allUsers = await prisma.user.findMany()
+    //const allUsers = await prisma.user.findMany()
 
-    res.json({message:"all users fetched successfully",users:allUsers});
+    res.json({message:"all users fetched successfully"/*,users:allUsers*/});
 });
 
 
